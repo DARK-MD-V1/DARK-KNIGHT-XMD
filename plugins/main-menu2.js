07fcb6c756337c035a281c9938bc38f180bcdf6c
bot-deploy-main <152987682+bot-deploy-main@users.noreply.github.com>
2025-06-27 22:06:44 +0530

    Update main-menu2.js

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -335,7 +335,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: config.MENU_IMAGE_URL || 'https://files.catbox.moe/lbde9t.jpg' },
+                image: { url: config.MENU_IMAGE_URL || 'https://files.catbox.moe/brlkte.jpg' },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
