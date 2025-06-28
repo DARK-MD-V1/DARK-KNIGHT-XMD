14736eee515582b4a06c6e2446bd4c7a536ba9d5
bot-deploy-main <152987682+bot-deploy-main@users.noreply.github.com>
2025-06-28 15:38:32 +0530

    Update ai-chat.js

--- plugins/ai-chat.js
+++ plugins/ai-chat.js
@@ -89,3 +89,19 @@ async (conn, mek, m, { from, args, q, reply, react }) => {
 });
 
 
+cmd({
+    pattern: "gemini",
+    react: "📑",
+    desc: "ai chat.",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let data = await fetchJson(`https://dark-shan-yt.koyeb.app/ai/gemini?q=${q}`)
+return reply(`${data.data}\n\n> ꜰᴏʀᴡᴀʀᴅ ʙʏ 𝙳𝙰𝚁𝙺-𝙺𝙽𝙸𝙶𝙷𝚃-𝚇𝙼𝙳`)
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+});
