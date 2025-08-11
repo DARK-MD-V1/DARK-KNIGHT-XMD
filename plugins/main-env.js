ea285ef5cee2cb821383cc4c0ea2160d5692a195
bot-deploy-main <152987682+bot-deploy-main@users.noreply.github.com>
2025-08-11 22:12:22 +0530

    Update main-env.js

--- plugins/main-env.js
+++ plugins/main-env.js
@@ -135,6 +135,21 @@ async (conn, mek, m, { from, quoted, reply, isCreator }) => {
 ╰──『 ${config.DESCRIPTION} 』──❏
 `;
 
+        // Fake VCard
+        const FakeVCard = {
+      key: {
+        fromMe: false,
+        participant: "0@s.whatsapp.net",
+        remoteJid: "status@broadcast"
+      },
+      message: {
+        contactMessage: {
+          displayName: "© 𝙳𝙰𝚁𝙺-𝙺𝙽𝙸𝙶𝙷𝚃",
+          vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:Meta\nORG:META AI;\nTEL;type=CELL;type=VOICE;waid=13135550002:+13135550002\nEND:VCARD`
+        }
+      }
+    }; 
+        
         await conn.sendMessage(
             from,
             {
@@ -146,7 +161,7 @@ async (conn, mek, m, { from, quoted, reply, isCreator }) => {
                     isForwarded: true
                 }
             },
-            { quoted: mek });
+            { quoted: FakeVCard });
 
     } catch (error) {
         console.error('Env command error:', error);
