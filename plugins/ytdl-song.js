f6fbd890e4e74fc2b5cc3905d38ca844e28b0838
bot-deploy-main <152987682+bot-deploy-main@users.noreply.github.com>
2025-09-11 21:00:56 +0530

    Update ytdl-song.js

--- plugins/ytdl-song.js
+++ plugins/ytdl-song.js
@@ -3,7 +3,7 @@ const yts = require('yt-search');
 const axios = require("axios");
 
 cmd({
-    pattern: "song2",
+    pattern: "song3",
     alias: ["ytmp3"],
     desc: "To download songs.",
     react: "🎵",
