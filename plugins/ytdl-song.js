567bf88d13da49041016efbd62fcd14516819732
bot-deploy-main <152987682+bot-deploy-main@users.noreply.github.com>
2025-09-26 10:03:19 +0530

    Update ytdl-song.js

--- plugins/ytdl-song.js
+++ plugins/ytdl-song.js
@@ -95,7 +95,7 @@ try {
     await conn.sendMessage(from, { image: { url: daa.thumbnail }, caption: desc }, { quoted: mek });
 
     // Use new API
-    let apiRes = await fetch(`https://api.giftedtech.web.id/api/download/ytmp4?apikey=gifted&url=${encodeURIComponent(url)}`);
+    let apiRes = await fetch(`https://api.giftedtech.web.id/api/download/dlmp4?apikey=gifted&url=${encodeURIComponent(url)}`);
     let json = await apiRes.json();
 
     if (!json.success) return reply("Failed to fetch video from new API");
