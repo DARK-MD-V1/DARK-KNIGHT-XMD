250e1793d85e35e3e9b7a83c618d873bb5804a7e
bot-deploy-main <152987682+bot-deploy-main@users.noreply.github.com>
2025-10-08 19:57:46 +0530

    Update ytdl-song-video1.js

--- plugins/ytdl-song-video1.js
+++ plugins/ytdl-song-video1.js
@@ -115,3 +115,64 @@ try {
     reply(`❌ An error occurred`);
 }
 })
+
+
+cmd({
+    pattern: "song11",
+    react: "🎵",
+    desc: "Download YouTube MP3",
+    category: "download",
+    use: ".song1 <query>",
+    filename: __filename
+}, async (conn, mek, m, { from, reply, q }) => {
+    try {
+        if (!q) return reply("❓ What song do you want to download?");
+
+        const yts = require('yt-search');
+        const search = await yts(q);
+        
+        if (!search.videos.length) return reply("❌ No results found for your query.");
+
+        const data = search.videos[0];
+        const ytUrl = data.url;
+        const ago = data.ago;
+
+        const api = `https://sadiya-tech-apis.vercel.app/download/ytdl?url=${ytUrl}&format=360&apikey=sadiya`;
+        const { data: apiRes } = await axios.get(api);
+
+        if (!apiRes?.status || !apiRes.result?.download) {
+            return reply("❌ ගීතය බාගත කළ නොහැක. වෙනත් එකක් උත්සහ කරන්න!");
+        }
+
+        const result = apiRes.result;
+
+        await conn.sendMessage(from, {
+            image: { url: result.thumbnail },
+            caption: `╸╸╸╸╸╸╸╸╸╸╸╸╸
+        
+ℹ️ *Title :* ${data.title}
+⏱️ *Duration :* ${data.timestamp} 
+🧬 *Views :* ${data.views}
+📅 *Released Date :* ${data.ago}
+🖇️ *Link :* ${data.url}
+ 
+╸╸╸╸╸╸╸╸╸╸╸╸╸
+
+> Powered by 𝙳𝙰𝚁𝙺-𝙺𝙽𝙸𝙶𝙷𝚃-𝚇𝙼𝙳`
+        }, { quoted: mek });
+
+        await conn.sendMessage(from, {
+            video: { url: result.download },
+            mimetype: "video/mp4",
+        }, { quoted: mek });
+       
+        await conn.sendMessage(from, {
+            document : { url: result.download },
+            mimetype: "video/mp4",
+            fileName: `${data.title}.mp4`
+        }, { quoted: mek });        
+
+    } catch (error) {
+        reply(`❌ An error occurred: ${error.message}`);
+    }
+});
