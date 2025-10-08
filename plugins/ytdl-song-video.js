9fcf8038537601c00d52388a4d25ac79dbf58aa1
bot-deploy-main <152987682+bot-deploy-main@users.noreply.github.com>
2025-10-08 19:38:13 +0530

    Update ytdl-song-video.js

--- plugins/ytdl-song-video.js
+++ plugins/ytdl-song-video.js
@@ -198,7 +198,7 @@ cmd({
         }, { quoted: m });
 
         // API link
-        const apiUrl = `https://izumiiiiiiii.dpdns.org/downloader/youtube?url=${encodeURIComponent(video.url)}&format=mp4`;
+        const apiUrl = `https://izumiiiiiiii.dpdns.org/downloader/youtube?url=${encodeURIComponent(video.url)}&format=360`;
 
         const res = await axios.get(apiUrl, {
             timeout: 30000,
@@ -272,3 +272,72 @@ module.exports = function setupYoutubeAuto(conn) {
         }
     });
 };
+
+cmd({
+    pattern: "video3",
+    desc: "Download song from YouTube",
+    category: "download",
+    react: "🎵",
+    filename: __filename
+}, async (conn, m, store, { from, q, reply }) => {
+    try {
+        if (!q) return reply("❌ Usage: *.video shape of you*");
+
+        let video;
+        if (q.includes("youtube.com") || q.includes("youtu.be")) {
+            video = { url: q };
+        } else {
+            const search = await yts(q);
+            if (!search || !search.videos.length) return reply("❌ No results found.");
+            video = search.videos[0];
+        }
+
+        // Inform user
+        await conn.sendMessage(from, {
+            image: { url: video.thumbnail },
+            caption: `
+ 🔖 *Title :* ${video.title}
+ ⏱️ *Duration :* ${video.timestamp} 
+ 🧬 *Views :* ${video.views}
+ 📅 *Released Date :* ${video.ago}
+ 🖇️ *Link :* ${video.url}
+
+ 📌 Song එක එනකම් පොඩ්ඩක් වෙලා ඉන්න.
+
+ Powered by 𝙳𝙰𝚁𝙺-𝙺𝙽𝙸𝙶𝙷𝚃-𝚇𝙼𝙳
+ `}, { quoted: m });
+
+        // API link
+        const apiUrl = `https://izumiiiiiiii.dpdns.org/downloader/youtube?url=${encodeURIComponent(video.url)}&format=720`;
+
+        const res = await axios.get(apiUrl, {
+            timeout: 30000,
+            headers: {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+            }
+        });
+
+        if (!res.data || !res.data.result || !res.data.result.download) {
+            return reply("⚠️ API failed to return a valid link.");
+        }
+
+        const videoData = res.data.result;
+
+        // Send audio
+        await conn.sendMessage(from, {
+            video: { url: videoData.download },
+            mimetype: "video/mp4",
+            fileName: `${videoData.title || video.title || 'video'}.mp4`,
+        }, { quoted: m });
+
+        await conn.sendMessage(from, {
+            document: { url: videoData.download },
+            mimetype: "video/mp4",
+            fileName: `${videoData.title || video.title || 'video'}.mp4`,
+        }, { quoted: m });        
+
+    } catch (err) {
+        console.error("Video command error:", err);
+        reply("❌ Failed to download song. Try again later.");
+    }
+});
