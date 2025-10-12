844d9a0e3c56491efd17cb8746bb63cdadd435d5
bot-deploy-main <152987682+bot-deploy-main@users.noreply.github.com>
2025-10-12 22:46:50 +0530

    Update main-menu1.js

--- plugins/main-menu1.js
+++ plugins/main-menu1.js
@@ -90,17 +90,19 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 ┃★│ • tt2
 ┃★│ • tiks
 ┃★│ • twitter
+┃★│ • ytmp3
 ┃★│ • song
 ┃★│ • song1
 ┃★│ • song2
 ┃★│ • song3
 ┃★│ • song4
+┃★│ • song5
+┃★│ • song6
 ┃★│ • video
 ┃★│ • video1
 ┃★│ • video2
 ┃★│ • video3
 ┃★│ • video4
-┃★│ • video5
 ┃★│ • xnxx
 ┃★│ • xvideo
 ┃★╰──────────────
