5974bb6399e4f49e49ac17a53f0dda0417f3b55d
bot-deploy-main <152987682+bot-deploy-main@users.noreply.github.com>
2025-10-14 18:37:40 +0530

    Create search-sticker.js

---
+++ plugins/search-sticker.js
@@ -0,0 +1,54 @@
+const { cmd } = require("../command");
+const axios = require("axios");
+
+cmd({
+    pattern: "searchsti",
+    react: "🦋",
+    desc: "Search and download Google images",
+    category: "download",
+    use: ".searchsti <keywords>",
+    filename: __filename
+}, async (conn, mek, m, { reply, args, from }) => {
+    try {
+        const query = args.join(" ");
+        if (!query) {
+            return reply("🦋 Please provide a search query\nExample: .searchsti cats");
+        }
+
+        await reply(`🔍 Searching Stickers for *"${query}"*...`);
+
+        const url = `https://supun-md-api-rho.vercel.app/api/search/sticker?q=${encodeURIComponent(query)}`;
+        const response = await axios.get(url);
+
+        if (!response.data?.success || !response.data.results?.length) {
+            return reply("❌ No images found. Try different keywords.");
+        }
+
+        const results = response.data.results;
+        await reply(`✅ Found *${results.length}* results for *"${query}"*. Sending top 10...`);
+
+        const selectedImages = results
+            .sort(() => 0.10 - Math.random())
+            .slice(0, 10);
+
+        for (const thumbnailUrl of selectedImages) {
+            try {
+                await conn.sendMessage(
+                    from,
+                    {
+                        image: { url: thumbnailUrl }
+                    },
+                    { quoted: mek }
+                );
+            } catch (err) {
+                console.warn(`⚠️ Failed to send Sticker: ${thumbnailUrl}`);
+            }
+
+            await new Promise(resolve => setTimeout(resolve, 1000));
+        }
+
+    } catch (error) {
+        console.error('Sticker Search Error:', error);
+        reply(`❌ Error: ${error.message || "Failed to fetch sticker"}`);
+    }
+});
