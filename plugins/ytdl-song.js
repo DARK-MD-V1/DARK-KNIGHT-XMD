76579cd36dadb00cfbffb2792a321fc1b5b0802b
bot-deploy-main <152987682+bot-deploy-main@users.noreply.github.com>
2025-10-24 08:00:08 +0530

    Update ytdl-song.js

--- plugins/ytdl-song.js
+++ plugins/ytdl-song.js
@@ -46,7 +46,7 @@ cmd({
 > Powered by 𝙳𝙰𝚁𝙺-𝙺𝙽𝙸𝙶𝙷𝚃-𝚇𝙼𝙳`;
 
         const sentMsg = await conn.sendMessage(from, {
-            image: { url: result.thumbnail },
+            image: { url: data.thumbnail },
             caption
         }, { quoted: m });
 
